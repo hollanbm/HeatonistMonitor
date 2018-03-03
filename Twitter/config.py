c68dd7549700a1a0f12810364dea644600ee2f37
Brad Holland <bholland@hchb.com>
2018-03-03 01:36:10 -0500

    update dockerfile

--- Twitter/config.py
+++ Twitter/config.py
@@ -4,7 +4,7 @@
 #	https://apps.twitter.com/app/new
 #--------------------------------------------------------------------------------
 
-consumer_key = "qHARQ5lgzoAoO4Jyp2qZmC8pA"
-consumer_secret = "XwNQltp4kgpcsdjh7og91snM5bDukLYv9WiG4t6xHhTi9tPbzz"
-access_key = "969803539538219008-vrbP5hwAzjn4v4EXdlcJjNbwdObtUS3"
-access_secret = "nfWdrqzABN4EQ2Ahxrl2NYrMvBg7PeTWNmcMCNtHbLCX7"
+consumer_key = "XxXxXxxXXXxxxxXXXxXX"
+consumer_secret = "xXXXXXXXXxxxxXxXXxxXxxXXxXxXxxxxXxXXxxxXXx"
+access_key = "XXXXXXXX-xxXXxXXxxXxxxXxXXxXxXxXxxxXxxxxXxXXxXxxXX"
+access_secret = "XxXXXXXXXXxxxXXXxXXxXxXxxXXXXXxXxxXXXXx"
